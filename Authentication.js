7d9ab2c5715d95ae185c99239522b78c40979694
Yongware <42880886+Yongware@users.noreply.github.com>
2020-07-08 10:40:50 +0900

    Update Authentication.js

--- Authentication.js
+++ Authentication.js
@@ -1,52 +1,57 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {View, Text, TextInput, Image, StyleSheet} from "react-native";
 import CustomButton from "./CustomButton"
+import { AntDesign } from '@expo/vector-icons';
 
-const loginmodule = require('skku-login-module')
-
-class Authentication extends React.Component{
+export default class Authentication extends React.Component{
     state = {
         id:"",
         pw:""
     };
 
-    handleChange = (e) => {
-        let nextState = {};
-        nextState[e.target.name] = e.target.value;
-        this.setState(nextState);
+    handleId = text => {
+        this.setState({id:text});
+    };
+
+    handlePw = text => {
+        this.setState({pw:text});
     }
 
-    render(){
-        const inputBoxes = (
-            <View>
-                <View style = {styles.userInput}>
-                    <label>아이디를 입력하세요 </label>
-                    <input
-                    name="id"
-                    type="text"
-                    onChange={this.handleChange}
-                    value = {this.state.id}
-                    />
-                </View>
-                <View style = {styles.userInput}>
-                    <label>비밀번호를 입력하세요</label>
-                    <input
-                    name="pw"
-                    type="password"
-                    onChange={this.handleChange}
-                    value={this.state.pw}
-                    />
-                </View>
-            </View>
-        );
+    login = (id, pw) =>{
+        alert("id: "+ id + "pw: "+pw);
+    };
 
+    render(){
         return (
             <View style = {styles.container}>
                 <Image style = {styles.logo} source = {require(`./logo.jpg`)}/>
-                {inputBoxes}
+                <View style = {styles.idContainer}>
+                    <AntDesign style = {styles.Icon} name="user" size={20} />
+                    <TextInput
+                    name="id"
+                    type="text"
+                    style = {styles.userInput}
+                    placeholder = "아이디를 입력하세요"
+                    onChangeText={this.handleId}
+                    //value = {this.state.id}
+                    />
+                </View>
+                <View style = {styles.pwContainer}>
+                    <AntDesign style = {styles.Icon} name="lock" size={20} />
+                    <TextInput
+                    name="pw"
+                    type="password"
+                    secureTextEntry= {true}
+                    style = {styles.userInput}
+                    placeholder = "비밀번호를 입력하세요"
+                    onChangeText={this.handlePw}
+                    //value={this.state.pw}
+                    />
+                </View>
                 <CustomButton
                 title={'LOGIN'}
-                onPress = {loginmodule.login_confirm(this.id, this.pw, (result)=>{ })}
+                onPress = {()=>this.login(this.state.id, this.state.pw)}
                 />
             </View>
         );
@@ -59,7 +64,20 @@ const styles = StyleSheet.create({
         padding: 10,
         backgroundColor: "white"
     },
-
+    idContainer:{
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderBottomColor: 'green',
+        height: 50,
+        paddingBottom:2,
+    },
+    pwContainer:{
+        flexDirection: 'row',
+        borderBottomWidth: 1,
+        borderBottomColor: 'green',
+        height: 50,
+        paddingBottom:2,
+    },
     logo:{
         width:'80%',
         height: '30%',
@@ -68,9 +86,16 @@ const styles = StyleSheet.create({
     },
 
     userInput:{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: "flex-end",
+        alignItems: "flex-end",
         backgroundColor: "white"
     },
-})
+
+    Icon:{
+        paddingTop: 15,
+        paddingRight: 10,
+        color: "grey",
+        justifyContent: "center",
+        alignItems: "baseline",
+    },
+});
