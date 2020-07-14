37c1215e972c8d9b9b40fa3b607dee7440d406dd
Yongware <42880886+Yongware@users.noreply.github.com>
2020-07-14 09:58:07 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,120 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, TextInput, Image, View } from 'react-native';
+import { StyleSheet, TextInput, Image, View, TouchableHighlight, Text } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { AntDesign } from '@expo/vector-icons';
 import CustomButton from "./src/components/CustomButton"
 import MainScreen from './src/screens/MainScreen';
+import TouchId from 'react-native-touch-id';
+import FingerprintScanner from 'react-native-fingerprint-scanner';
 
+
+class Fingerprint extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorMessageLegacy: undefined,
+      biometricLegacy: undefined
+    };
+ 
+    this.description = null;
+  }
+ 
+  componentDidMount() {
+    if (this.requiresLegacyAuthentication()) {
+      this.authLegacy();
+    } else {
+      this.authCurrent();
+    }
+  }
+ 
+  componentWillUnmount = () => {
+    FingerprintScanner.release();
+  }
+ 
+  requiresLegacyAuthentication() {
+    return Platform.Version < 23;
+  }
+ 
+  authCurrent() {
+    FingerprintScanner
+      .authenticate({ title: 'Log in with Biometrics' })
+      .then(() => {
+        this.props.onAuthenticate();
+      });
+  }
+ 
+  authLegacy() {
+    FingerprintScanner
+      .authenticate({ onAttempt: this.handleAuthenticationAttemptedLegacy })
+      .then(() => {
+        this.props.handlePopupDismissedLegacy();
+        Alert.alert('Fingerprint Authentication', 'Authenticated successfully');
+      })
+      .catch((error) => {
+        this.setState({ errorMessageLegacy: error.message, biometricLegacy: error.biometric });
+        this.description.shake();
+      });
+  }
+ 
+  handleAuthenticationAttemptedLegacy = (error) => {
+    this.setState({ errorMessageLegacy: error.message });
+    this.description.shake();
+  };
+ 
+  renderLegacy() {
+    const { errorMessageLegacy, biometricLegacy } = this.state;
+    const { style, handlePopupDismissedLegacy } = this.props;
+ 
+    return (
+      <View style={styles.container}>
+        <View style={[styles.contentContainer, style]}>
+ 
+          <Image
+            style={styles.logo}
+            source={require('./assets/finger_print.png')}
+          />
+ 
+          <Text style={styles.heading}>
+            Biometric{'\n'}Authentication
+          </Text>
+          <ShakingText
+            ref={(instance) => { this.description = instance; }}
+            style={styles.description(!!errorMessageLegacy)}>
+            {errorMessageLegacy || `Scan your ${biometricLegacy} on the\ndevice scanner to continue`}
+          </ShakingText>
+ 
+          <TouchableOpacity
+            style={styles.buttonContainer}
+            onPress={handlePopupDismissedLegacy}
+          >
+            <Text style={styles.buttonText}>
+              BACK TO MAIN
+            </Text>
+          </TouchableOpacity>
+ 
+        </View>
+      </View>
+    );
+  }
+
+   
+ 
+  render = () => {
+    if (this.requiresLegacyAuthentication()) {
+      return this.renderLegacy();
+    }
+ 
+    // current API UI provided by native BiometricPrompt
+    return null;
+  }
+}
 
 class Login extends React.Component{
   state = {
     id:"",
-    pw:""
+    pw:"",
   };
 
   handleId = text => {
@@ -24,37 +126,37 @@ class Login extends React.Component{
   };
 
   login = (id, pw) =>{
-    alert("id: "+ id + "pw: "+pw);
-    this.props.navigation.push('SKKUDID.MainScreen')
+    alert("id: "+ id + ", pw: "+pw);
+    this.props.navigation.push('MainScreen')
   };
 
   render(){
     return(
       <View style={styles.container}>
-        <View style = {styles.container}>
-          <Image style = {styles.logo} source = {require(`./logo.jpg`)}/>
-            <View style = {styles.idContainer}>
-              <AntDesign style = {styles.Icon} name="user" size={20} />
-              <TextInput
-              name="id"
-              type="text"
-              style = {styles.userInput}
-              placeholder = "아이디를 입력하세요"
-              onChangeText={this.handleId}
-              //value = {this.state.id}
-              />
-            </View>
-            <View style = {styles.pwContainer}>
-              <AntDesign style = {styles.Icon} name="lock" size={20} />
-              <TextInput
-              name="pw"
-              type="password"
-              secureTextEntry= {true}
-              style = {styles.userInput}
-              placeholder = "비밀번호를 입력하세요"
-              onChangeText={this.handlePw}
-              //value={this.state.pw}
-              />
+        <View>
+          <Image style = {styles.logo} source = {require(`./colorLogo.jpg`)}/>
+          <View style = {styles.idContainer}>
+            <AntDesign style = {styles.Icon} name="user" size={20} />
+            <TextInput
+            name="id"
+            type="text"
+            style = {styles.userInput}
+            placeholder = "아이디를 입력하세요"
+            onChangeText={this.handleId}
+            //value = {this.state.id}
+            />
+          </View>
+          <View style = {styles.pwContainer}>
+            <AntDesign style = {styles.Icon} name="lock" size={20} />
+            <TextInput
+            name="pw"
+            type="password"
+            secureTextEntry= {true}
+            style = {styles.userInput}
+            placeholder = "비밀번호를 입력하세요"
+            onChangeText={this.handlePw}
+            //value={this.state.pw}
+            />
           </View>
           <CustomButton
           title={'LOGIN'}
@@ -66,26 +168,28 @@ class Login extends React.Component{
   }
 }
 
-Navigation.registerComponent('SKKUDID.LoginScreen', ()=> Login);
-Navigation.registerComponent('SKKUDID.MainScreen', ()=> MainScreen);
+Navigation.registerComponent('Authentication', ()=> Fingerprint)
+Navigation.registerComponent('SKKUDID', ()=> Login);
+Navigation.registerComponent('MainScreen', ()=> MainScreen);
 
 const AppNavigator = createStackNavigator(
  {
-    'SKKUDID.LoginScreen': Login,
-    'SKKUDID.MainScreen' : MainScreen
+    'Authentication': Fingerprint,
+    'SKKUDID': Login,
+    'MainScreen' : MainScreen
  },
  {
-   initialRouteName: 'SKKUDID.LoginScreen',
+   initialRouteName: 'SKKUDID',
  }
 );
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "white"
+    flex: 2,
+    padding: 20,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
   },
   idContainer:{
       flexDirection: 'row',
@@ -102,10 +206,8 @@ const styles = StyleSheet.create({
       paddingBottom:2,
   },
   logo:{
-      width:'80%',
-      height: '30%',
-      width:300,
-      height:300
+    width:300,
+    height:300,
   },
 
   userInput:{
