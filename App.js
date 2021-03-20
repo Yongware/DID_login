a37d102871d8e68be79aa4ee726ef88c87914c57
YONGWOO CHOI <42880886+cyw320712@users.noreply.github.com>
2021-03-20 23:16:42 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -6,18 +6,27 @@ import RegisterScreen from './src/screens/RegisterScreen';
 import AgeQRScreen from './src/screens/AgeQRScreen';
 import SKKUQRScreen from './src/screens/SKKUQRScreen';
 import LibraryQRScreen from './src/screens/LibraryQRScreen';
+import PinScreen from './src/screens/PinScreen';
+import PinSetScreen from './src/screens/PinSetScreen';
+import CurrentPinCheck from './src/screens/CurrentPinCheck';
+import NewPinCheck from './src/screens/NewPinCheckScr';
 
 const AppNavigator = createStackNavigator(
-  {
+  {//Navigator에 연결된 화면 및 각각을 지칭하는 이름 리스트
+    //Navigator에선 이곳에서 지정된 이름으로 각 화면 호출
+    'PIN':PinScreen,
     'Login':LoginScreen,
     'Main':MainScreen,
     'Register':RegisterScreen,
     'AgeCheck':AgeQRScreen,
     'SKKUCheck':SKKUQRScreen,
-    'LibraryCheck': LibraryQRScreen
+    'LibraryCheck': LibraryQRScreen,
+    'CurrentPinCheck':CurrentPinCheck,
+    'PinSetting':PinSetScreen,
+    'NewPinCheck':NewPinCheck,
   },
-  {
-    initialRouteName: 'Login',
+  {//초기화면(PinScreen으로 시작)
+    initialRouteName: 'PIN',
   }
 );
 
